2f09efa4842fba859d8dcc14f5f52b7b4383e9aa
KarenAlvaradoS <159507240+KarenAlvaradoS@users.noreply.github.com>
2025-01-12 23:39:24 -0500

    Create EJERCICIO_1.cs
    Almacenar las asignaturas en una lista y mostrarlas por pantalla.

---
+++ EJERCICIO_1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> asignaturas = new List<string> { "Matemáticas", "Física", "Química", "Historia", "Lengua" };
+
+        Console.WriteLine("Asignaturas del curso:");
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine(asignatura);
+        }
+    }
+}
